880205548e1e22d5ce7202160602a3f6a29da346
Mukudzei Chisenwa <e3740138@student.dcccd.edu>
2025-02-05 23:22:50 -0600

    Init Commit

---
+++ script.js
@@ -0,0 +1,2 @@
+var guest = prompt("Welcome! This is a JavaScript Demo. See details at the end of the article. Please enter your name.");
+document.write("<h2>Welcome to JavaScript, " + guest + "!</h2>");
