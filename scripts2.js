61f3f2b0d1ec1cb7e3486a35eb31785496e29be2
Mukudzei Chisenwa <e3740138@student.dcccd.edu>
2025-02-17 16:48:49 -0600

    Website init

--- scripts2.js
+++ scripts2.js
@@ -1,3 +1,4 @@
+//QUOTES
 var quotes = ["The best way to predict the future is to invent it.", 
         "Life is 10% what happens to us and 90% how we react to it.",
         "Do what you can, with what you have, where you are.",
@@ -11,3 +12,16 @@ var index = number % 5;
 var remainder = quotes[index];
 
 document.write(remainder);
+
+//WEBSITE
+var favoriteWebsites = [
+    "https://www.google.com",
+    "https://www.wikipedia.org",
+    "https://www.github.com"
+];
+
+var newWebsite = prompt("Please enter your favourite website: ");
+
+favoriteWebsites.push("newWebsite");
+favoriteWebsites.shift()
+
